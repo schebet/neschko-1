5f7fb1679a78a980cdd9772fb292a6c1a1fb14e6
schebet <91207140+schebet@users.noreply.github.com>
2022-09-20 17:03:31 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>Video, čuo o raznim čudesima i razmišljao N.Đ &darr;</h2>
       <div className="grids">
         {Posts}
       </div>

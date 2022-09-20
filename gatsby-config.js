e1eef2dd48e1487abd7681bbb4b919b337aa13c3
schebet <91207140+schebet@users.noreply.github.com>
2022-09-20 18:48:03 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Delog GatbsyJS Starter`,
-        short_name: `Delog`,
+        name: `Neškov sajt`,
+        short_name: `Neško`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#381696`,
